94a367a72602e6aa5832ef9d593a7cc19b08e2d1
bobJiang <374123140@qq.com>
2024-10-11 18:00:18 +0800

    add 本地注册中心

--- provider/src/main/java/com/jmyok/SpringBootProviderMain.java
+++ provider/src/main/java/com/jmyok/SpringBootProviderMain.java
@@ -1,13 +1,17 @@
 package com.jmyok;
 
+import com.jmyok.common.service.UserService;
+import com.jmyok.rpc.registry.LocalRegistry;
 import com.jmyok.rpc.server.HttpServer;
 import com.jmyok.rpc.server.VertxHttpServer;
 
 public class SpringBootProviderMain {
 
     public static void main(String[] args) {
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
+
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
-
     }
 }
